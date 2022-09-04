616f6bfd9b1e4bfc055173da8ba5c64d3ac2eb0e
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-04 19:21:18 +0300

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
-        System.out.print(number%100);
+        System.out.print(number%10);
         number /= 10;
         System.out.print(number%10);
         number /= 10;
